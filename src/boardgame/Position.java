fb9e1d8690d5be8c5e3b4ae81bc36f895eadb393
HDeze7 <heytormartins17@outlook.com>
2021-03-12 16:35:56 -0300

    FirstClass: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+	
+	private int Row;
+	private int Column;
+	
+	public Position(int row, int column) {
+		Row = row;
+		Column = column;
+	}
+
+	public int getRow() {
+		return Row;
+	}
+
+	public void setRow(int row) {
+		Row = row;
+	}
+
+	public int getColumn() {
+		return Column;
+	}
+
+	public void setColumn(int column) {
+		Column = column;
+	}
+
+	@Override
+	public String toString() {
+		return "Position [Row= " + Row + ", Column= " + Column + "]";
+	}	
+}
