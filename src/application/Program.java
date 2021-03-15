b096598232bbca8fc657ba1eee7043dfac07fbb3
HDeze7 <heytormartins17@outlook.com>
2021-03-15 14:56:30 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -14,16 +16,27 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();//Aqui eu instanciei uma partida de xadrez e iremos imprimir o tabuleiro dessa partida
 		
 		while(true) {
-		UI.printBoard(chessMatch.getPieces());//Essa é uma função para imprimir as peças da partida. Pra fazer isso eu vou criar uma classe chamada UI(UserInterface), e dentro dessa classe, eu vou criar um método chamado printBoard "UI.printBoard", esse método vai receber a matriz de peças da minha partida "(chessMatch.getPieces());"
-		System.out.println();
-		System.out.print("Source: ");
-		ChessPosition source = UI.readChessPosition(sc);
-		
-		System.out.println();
-		System.out.print("Target: ");
-		ChessPosition target = UI.readChessPosition(sc);
-		
-		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-		} 
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());//Essa é uma função para imprimir as peças da partida. Pra fazer isso eu vou criar uma classe chamada UI(UserInterface), e dentro dessa classe, eu vou criar um método chamado printBoard "UI.printBoard", esse método vai receber a matriz de peças da minha partida "(chessMatch.getPieces());"
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} 
+			catch(ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
 	}
 }
